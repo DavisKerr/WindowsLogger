3594e6444541ff61f511fcb2e1918f36bf9cb9da
Davis Kerr <daviskerr@byui.edu>
2021-11-29 14:36:19 -0700

    Add project files.

---
+++ WindowsLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WindowsLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
