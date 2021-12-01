7adcb3b73d275bddc6db0781c54c41101ec9dcba
Davis Kerr <daviskerr@byui.edu>
2021-12-01 13:57:34 -0700

    Added the LogReader class

---
+++ WindowsLogger/EventLogger/EventLogReader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using WindowsLogger;
+using System.Threading;
+
+namespace WindowsLogger.EventLogger
+{
+    class EventLogReader
+    {
+        EventLog eventLog = new EventLog();
+
+        public EventLogReader()
+        {
+
+        }
+        public void run()
+        {
+            string option = "1";
+
+            while(option != "0")
+            {
+                Console.WriteLine("Which feature would you like to use?");
+                Console.WriteLine("(0) - quit");
+                Console.WriteLine("(1) - retrieve login data");
+                Console.WriteLine("(2) - retrieve login data for a specific user");
+
+                option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "0":
+                        Console.WriteLine("exiting logger...");
+                        break;
+                    case "1":
+                        getLoginData(100);
+                        break;
+                    case "2":
+                        Console.WriteLine("Not implemented");
+                        break;
+                    default:
+                        Console.WriteLine("Command not recognized.");
+                        break;
+                }
+            }
+        }
+
+        public void getLoginData(int limit)
+        {
+            eventLog.Log = "Security";
+
+            
+
+            foreach (EventLogEntry entry in eventLog.Entries)
+            {
+                if(entry.Index < limit)
+                {
+                    Console.WriteLine("User: " + entry.UserName + "\n");
+                    Console.WriteLine("Time: " + entry.TimeWritten + "\n");
+                    Console.WriteLine("Message: " + entry.Message + "\n");
+                    Console.WriteLine("--------------------------------\n");
+                }
+                
+
+                
+            }
+        }
+    }
+}
--- WindowsLogger/Program.cs
+++ WindowsLogger/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/* When setting up the project, run 
+ * Install-Package Microsoft.Extensions.Logging.EventLog
+ * in the NuGet terminal
+ * 
+ * 
+ */
+using System;
+using System.Diagnostics;
+using WindowsLogger;
+using System.Threading;
+using WindowsLogger.EventLogger;
 
 namespace WindowsLogger
 {
@@ -6,7 +16,39 @@ namespace WindowsLogger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to windows logger!");
+            string option = "1";
+
+            while(option != "0")
+            {
+                Console.WriteLine("Which feature would you like to use?");
+                Console.WriteLine("(0) - quit");
+                Console.WriteLine("(1) - Event Viewer");
+                Console.WriteLine("(2) - KeyLogger");
+
+                option = Console.ReadLine();
+
+                if(option == "0")
+                {
+                    Console.WriteLine("Exiting...");
+                }
+                else if(option == "1")
+                {
+                    EventLogReader reader = new EventLogReader();
+                    reader.run();
+                }
+                else if(option == "2")
+                {
+                    Console.WriteLine("Not Implemented");
+                }
+                else
+                {
+                    Console.WriteLine("Command not recognized. Try again.");
+                }
+            }
+            
+
+
         }
     }
 }
